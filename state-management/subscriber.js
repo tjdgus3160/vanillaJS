4a00fd0f76ba022f106f5070c2faa2877075ad58
tjdgus3160 <tjdgus3160@gmail.com>
2022-08-11 19:14:48 +0900

    observer patteren : subscriber 추가

---
+++ state-management/subscriber.js
@@ -0,0 +1,12 @@
+class Subscriber {
+  #fn;
+
+  constructor(subscribe) {
+    // subscribe : 상태 변화 시 실행할 함수
+    this.#fn = subscribe;
+  }
+
+  subscriber(publisher) {
+    publisher.addSubscriber(this.#fn);
+  }
+}

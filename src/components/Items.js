6ce9951116794984c93d28dda59b61d02e806705
tjdgus3160 <tjdgus3160@gmail.com>
2022-08-11 14:17:32 +0900

    컴포넌트 모듈화

---
+++ src/components/Items.js
@@ -0,0 +1,26 @@
+import Component from "../core/Component.js";
+
+export default class Items extends Component {
+  setup() {
+    this.$state = { items: ["item1", "item2"] };
+  }
+
+  template() {
+    const { items } = this.$state;
+
+    return `
+          <ul>
+            ${items.map((item) => `<li>${item}</li>`).join("")}
+          </ul>
+          <button id="append">추가</button>
+        `;
+  }
+
+  setEvent() {
+    document.querySelector("#append").addEventListener("click", () => {
+      const { items } = this.$state;
+
+      this.setState({ items: [...items, `item${items.length + 1}`] });
+    });
+  }
+}

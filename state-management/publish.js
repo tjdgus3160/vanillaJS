d12c5cf048e5fb12824bab7aee25fc10e8322cf2
tjdgus3160 <tjdgus3160@gmail.com>
2022-08-11 19:08:07 +0900

    observer pattern: publish 추가

---
+++ state-management/publish.js
@@ -0,0 +1,29 @@
+class Publish {
+  #state;
+  #observers = new Set();
+
+  constructor(state) {
+    this.#state = state;
+    Object.keys(state).forEach((key) =>
+      Object.defineProperty(this, key, {
+        get: () => this.#state[key],
+      })
+    );
+  }
+
+  // 내부에 변화가 생김
+  changeState(newState) {
+    this.#state = { ...this.#state, ...newState };
+    this.notify();
+  }
+
+  // 구독자 등록
+  addSubscriber(subscriber) {
+    this.#observers.add(subscriber);
+  }
+
+  // 구독자에게 알림
+  notify() {
+    this.#observers.forEach((fn) => fn());
+  }
+}

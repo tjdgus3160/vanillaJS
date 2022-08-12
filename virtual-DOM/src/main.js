b13cd702bcbfa5716d5d3816d075319d89ce141b
tjdgus3160 <tjdgus3160@gmail.com>
2022-08-12 18:00:04 +0900

    virtual-DOM : Diff 알고리즘 적용

--- virtual-DOM/src/main.js
+++ virtual-DOM/src/main.js
@@ -25,19 +25,82 @@ function createElement(node) {
   return $el;
 }
 
-const state = [
+function updateElement(parent, newNode, oldNode, index = 0) {
+  // oldNode만 있는 경우 : oldNode를 parent에서 제거
+  if (!newNode && oldNode) {
+    return parent.removeChild(parent.childNode[index]);
+  }
+
+  // newNode만 있는 경우 : newNode를 parent에 추가
+  if (newNode && !oldNode) {
+    return parent.appendChild(createElement(newNode));
+  }
+
+  // oldNode와 newNode 모두 text 타입인 경우 : 변경된 내용 수정
+  if (typeof newNode === "string" && typeof oldNode === "string") {
+    if (newNode === oldNode) {
+      return;
+    }
+
+    return parent.replaceChild(createElement(newNode), parent.childNodes[index]);
+  }
+
+  // oldNode와 newNode의 태그 이름(type)이 다른 경우 : oldNode 삭제, newNode 추가
+  if (newNode.type !== oldNode.type) {
+    return parent.replaceChild(createElement(newNode), parent.childNodes[index]);
+  }
+
+  // oldNode와 newNode 속성(attr) 맞추기
+  updateAttributes(parent.childNodes[index], newNode.props || {}, oldNode.props || {});
+
+  // newNode와 oldNode의 모든 자식 태그를 순회하며 내용 반복
+  const maxLength = Math.max(newNode.children.length, oldNode.children.length);
+
+  for (let i = 0; i < maxLength; i++) {
+    updateElement(parent.childNodes[index], newNode.children[i], oldNode.children[i], i);
+  }
+}
+
+// newNode와 oldNode의 attribute를 비교하여 변경된 부분 수정
+function updateAttributes(target, newProps, oldProps) {
+  // 달라지거나 추가된 Props를 반영
+  for (const [attr, value] of Object.entries(newProps)) {
+    if (oldProps[attr] === newProps[attr]) {
+      continue;
+    }
+
+    target.setAttribute(attr, value);
+  }
+
+  // 없어진 props를 attribute에서 제거
+  for (const attr of Object.keys(oldProps)) {
+    if (newProps.hasOwnProperty(attr)) {
+      continue;
+    }
+
+    target.removeAttribute(attr);
+  }
+}
+
+const oldState = [
   { id: 1, completed: false, content: "todo list item 1" },
   { id: 2, completed: true, content: "todo list item 2" },
 ];
 
-const el = createElement(
+const newState = [
+  { id: 1, completed: true, content: "todo list item 1 update" },
+  { id: 2, completed: true, content: "todo list item 2" },
+  { id: 3, completed: false, content: "todo list item 3" },
+];
+
+const render = (state) => (
   <div id="app">
     <ul>
       {state.map(({ completed, content }) => (
-        <li className={completed ? "completed" : null}>
-          <input type="checkbox" className="toggle" checked={completed} />
+        <li class={completed ? "completed" : null}>
+          <input type="checkbox" class="toggle" checked={completed} />
           {content}
-          <button className="remove">삭제</button>
+          <button class="remove">삭제</button>
         </li>
       ))}
     </ul>
@@ -48,6 +111,10 @@ const el = createElement(
   </div>
 );
 
-const $root = document.body.querySelector("#root");
+const oldNode = render(oldState);
+const newNode = render(newState);
 
-$root.appendChild(el);
+const $root = document.querySelector("#root");
+
+updateElement($root, oldNode);
+setTimeout(() => updateElement($root, newNode, oldNode), 1000); // 1초 뒤에 DOM 변경

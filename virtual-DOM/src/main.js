74e3e0c9f46b9c062290397f88fc022aaba705ae
tjdgus3160 <tjdgus3160@gmail.com>
2022-08-12 17:29:20 +0900

    virtual-DOM : createElement 추가

--- virtual-DOM/src/main.js
+++ virtual-DOM/src/main.js
@@ -3,12 +3,34 @@ function h(type, props, ...children) {
   return { type, props, children: children.flat() };
 }
 
+function createElement(node) {
+  if (typeof node === "string") {
+    // text node 반환
+    return document.createTextNode(node);
+  }
+
+  // tag element 생성
+  const $el = document.createElement(node.type);
+
+  // 속성 삽입
+  Object.entries(node.props || {})
+    .filter(([_, value]) => value)
+    .forEach(([attr, value]) => $el.setAttribute(attr, value));
+
+  // 재귀적으로 자식 노드들 dom으로 변환
+  if (node?.children) {
+    node.children.map(createElement).forEach((child) => $el.appendChild(child));
+  }
+
+  return $el;
+}
+
 const state = [
   { id: 1, completed: false, content: "todo list item 1" },
   { id: 2, completed: true, content: "todo list item 2" },
 ];
 
-const vm = (
+const el = createElement(
   <div id="app">
     <ul>
       {state.map(({ completed, content }) => (
@@ -28,6 +50,4 @@ const vm = (
 
 const $root = document.body.querySelector("#root");
 
-$root.innerHTML = `
-    <pre>${JSON.stringify(vm, null, 2)}</pre>
-  `;
+$root.appendChild(el);

05224edb166b28cd50d04d919f1219b315279fb2
tjdgus3160 <tjdgus3160@gmail.com>
2022-08-12 13:52:22 +0900

    observer pattern 최적화

--- state-management/observerPattern/src/core/observer.js
+++ state-management/observerPattern/src/core/observer.js
@@ -1,7 +1,16 @@
 let currentObserver = null;
 
+// 연속적으로 상태값이 바뀔경우 맨 마자믹만 적용
+const debouncdFrame = (callback) => {
+  let currentCallback = -1;
+  return () => {
+    cancelAnimationFrame(currentCallback); // 현재 등록된 callback 취소
+    currentCallback = requestAnimationFrame(callback); // 1프레임 뒤에 실행
+  };
+};
+
 export const observe = (fn) => {
-  currentObserver = fn;
+  currentObserver = debouncdFrame(fn);
   fn();
   currentObserver = null;
 };
@@ -23,6 +32,11 @@ export const observable = (obj) => {
         return _value;
       },
       set(value) {
+        // 값이 변경되지 않는 경우
+        if (_value === value || JSON.stringify(_value) === JSON.stringify(value)) {
+          return;
+        }
+
         _value = value;
 
         // observers에 등록된 모든 observer(fn)를 실행
